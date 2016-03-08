a0ad59c407562d1e0017225d01b01927476aa9ff
Ilian <ilian1996@hotmail.com>
2016-03-08 19:02:39 +0100

    Commiting our first file

--- src/testgitting.java
+++ src/testgitting.java
@@ -3,7 +3,7 @@
  */
 public class testgitting {
     public static void main(String args[]){
-        System.out.println("lek med mina bollar");
+        System.out.println("lek med mina bollar,dumbass");
     }
 
 }
