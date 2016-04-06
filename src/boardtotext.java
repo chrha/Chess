551cc0803df913f57911f910d6b5a26735231df7
Ilian <ilian1996@hotmail.com>
2016-04-06 15:32:09 +0200

    Commiting our first file

--- src/boardtotext.java
+++ src/boardtotext.java
@@ -1,46 +1,42 @@
+import Pieces.Piece;
+
 /**
  * Created by Ilian on 2016-04-05.
  */
 public class boardtotext {
-    public static String convertToText(Board board){
+    public static String convertToText(Board board) {
         StringBuilder boardtext = new StringBuilder();
-        for (int x=0;x<Board.SIZE;x++){
-            for (int y=0;y<Board.SIZE;y++){
-                switch(board.getPiece(x,y)){
-                    case EMPTY:
-                        boardtext.append("_ ");
-                        break;
-                    case WHITE_KING :
-                        boardtext.append("K");
-                        break;
-                    case I:
-                        boardtext.append("I ");
-                        break;
-                    case O:
-                        boardtext.append("O ");
-                        break;
-                    case T:
-                        boardtext.append("T ");
-                        break;
-                    case S:
-                        boardtext.append("S ");
-                        break;
-                    case Z:
-                        boardtext.append("Z ");
-                        break;
-                    case J:
-                        boardtext.append("J ");
-                        break;
-                    case L:
-                        boardtext.append("L ");
-                        break;
-                    default:
-                        boardtext.append("NULL ");
+        for (int y = 0; y < Board.SIZE; y++) {
+            for (int x = 0; x < Board.SIZE; x++) {
+                String p;
+                String u;
+                if (board.getPiece(x,y) == null){
+                    p = null;
+                    u = null;
+                }else{
+                    p = board.getPiece(x, y).getDescription();
+                    u= p.substring(0, Math.min(p.length(), 4));
                 }
+                if (p == null) {
+                    boardtext.append("E");
+                }else if (u.equals("King")) {
+                    boardtext.append("K");
+                }else if (u.equals("Quee")) {
+                    boardtext.append("Q");
+                }else if (u.equals("Bish")) {
+                    boardtext.append("B");
+                }else if (u.equals("Knig")) {
+                    boardtext.append("H");
+                }else if (u.equals("Rook")) {
+                    boardtext.append("R");
+                }else if (u.equals("Pawn")) {
+                    boardtext.append("P");
+                }
+
             }
+            boardtext.append('\n');
+
         }
-
-        return boardtext;
+        return boardtext.toString();
     }
-
 }
